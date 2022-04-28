643d05653e5217bcbb85af786e0aabb6f5d82a71
Ismail <ismailadekambi@gmail.com>
2022-04-28 10:08:47 +0100

    Initial commit

---
+++ boostrap note.js
@@ -0,0 +1,21 @@
+//container class must set to container-fluid
+//xs <=576px
+//sm <=
+
+/*
+container----removes margin around view port
+container-fluid ---contain the entire device width
+p-3-----padding round the content
+border----to declare border 
+bg-primary---to give background
+bg-success--green
+bg-danger--red
+bg-info---cyan
+bg-warning--yellow
+text-center---center text
+text-white----color text
+form-control----for input
+h-50----------specify height 50
+d-flex
+flex--column
+*/
